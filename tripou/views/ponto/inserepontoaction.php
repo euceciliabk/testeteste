9679beeeb8f96b4911b7973a3aecb90c1c741523
euceciliabk <51093324+euceciliabk@users.noreply.github.com>
2021-03-05 22:47:14 -0300

    Delete tripou directory

--- tripou/views/ponto/inserepontoaction.php
+++
@@ -1,9 +0,0 @@
-<?php
-chdir("../../");
-require_once "controle.php";
-$controlePonto = new criaControlePonto();
-$controlePonto->inserePonto($_POST['texto']);
-
-header("Location: /tripou/");
-
-?>
